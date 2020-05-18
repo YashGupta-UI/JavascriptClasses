bd6abf2e40571cf92e512ad86f34cd6d41cb9280
Yash Gupta <yash.guptacs92@gmail.com>
2020-05-18 08:21:44 +0530

    Day 1

---
+++ Day1/topics.js
@@ -0,0 +1,128 @@
+/* Javascript  Types 
+
+Number; %
+String; "" + "" , with number , negative number type coersion "heelo" - "bye" 10 -"3"
+Boolean; 3 > 2
+undefined;
+null;
+object;
+symbol;
+
+*/
+
+//type coersion
+
+/* Operators 
+
+==
+===
+!==
+<=
+>=
+>
+<
+
+*/
+
+/* Javascript Variables
+
+why do we need to store value in programming
+
+prompt() -> return string in case of number if want to add two number so use Number(first) + Number(second)
+show result in alert
+
+whats your username and say hello
+
+
+
+var 
+
+var a = true;
+a = hello;
+
+var b;
+console.log(b)
+
+console.log(c)
+
+
+
+let
+const
+
+*/
+
+/* Control Flow  Conditions + Logical Operator
+
+
+if
+else
+else if
+ternary
+switch
+
+&&
+||
+!
+
+*/
+
+/* How javascript implemented in broswer */
+
+/*
+our html get loader then it seens the src tag and pull the css file but how about javascript
+
+show index.html
+
+*/
+
+/* Functions
+
+alert , prompt thats the function
+js provide us some functions that we can use when developing something
+to execute the function we need to use ()
+
+function declaration examples
+function expression examples
+
+basis console example both then with parameters
+
+then tell the the scerio of return
+if else return
+
+
+*/
+
+/* Data Strcuture Arrays
+
+var list = ["",""]
+how to access
+push
+pop
+shift
+unshift
+length
+map
+foreach
+filter
+for loop
+sort
+
+
+ex -> var newList = [["bear","tiger","dog"]]
+get the tiger
+
+
+
+*/
+
+/* data Structure Objects Most Powerful
+
+In JS everyting is an object (Inheritance)
+
+obj ={
+
+}
+
+fetch some and methods
+*/
